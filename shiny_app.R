82ce53096ba735e81ec28e0ce8e1a39f757444b4
Tsunami <2385942202@qq.com>
2022-09-30 09:20:26 +0200

    complete shiny app

--- shiny_app.R
+++ shiny_app.R
@@ -3,10 +3,13 @@ library(shiny)
 library(shinythemes)
 library(ggplot2)
 
+#rmm<-get_data(link="http://api.kolada.se/v2/data/",kpi='N00945',period='2009')
+#dfdata<-return_df(rmm)
+#jsontext<-return_json(rmm)
 
 ui <- fluidPage(
   theme = shinytheme("lumen"),
-  titlePanel("GET DATA"),
+  titlePanel("GET DATA(You must fullfill 2 in 3 below)"),
   textInput(inputId="kpi","KPI",'N00945'),
   textInput(inputId="municipality","Municipality",''),
   textInput(inputId="period","Year",'2009'),
@@ -17,14 +20,22 @@ ui <- fluidPage(
 
 # Define server function
 server <- function(input, output) {
-  pkpi<-reactive({input$kpi})
-  pmunicipality<-reactive({input$municipality})
-  pperiod<-reactive({input$period})
+  #pkpi<-input$kpi
+  #pmunicipality<-input$municipality
+  #pperiod<-input$period
 
-  rmm<-get_data(link="http://api.kolada.se/v2/data/",kpi=pkpi,period=pperiod,municipality=pmunicipality)
-  dfdata<-return_df(rmm)
-  jsontext<-return_json(rmm)
-  output$df_data<-renderTable(dfdata)
+  #rmm<-get_data(link="http://api.kolada.se/v2/data/",kpi=pkpi,period=pperiod,municipality=pmunicipality)
+  #dfdata<-return_df(rmm)
+  #jsontext<-return_json(rmm)
+  output$df_data<-renderTable({
+    pkpi<-input$kpi
+    pmunicipality<-input$municipality
+    pperiod<-input$period
+    rmm<-get_data(link="http://api.kolada.se/v2/data/",kpi=pkpi,period=pperiod,municipality=pmunicipality)
+    dfdata<-return_df(rmm)
+    jsontext<-return_json(rmm)
+    print(dfdata)}
+                              )
 }
 
 
